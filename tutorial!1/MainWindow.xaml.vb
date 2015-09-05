c32cb19787950b8b225d755172ece1c7a00cb39c
mantis22 <anibal.fermandois@gmail.com>
2015-09-05 14:15:37 -0300

    inicio

---
+++ tutorial!1/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class MainWindow
+    Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
+
+
+        label1.Content = textBox1.Text + textBox2.Text
+
+
+    End Sub
+
+
+End Class
